28cd84c2fbad31255ff9b30b442362a4784a41e9
zweizhao <zweizhao@foxmail.com>
2019-01-29 12:11:46 +0800

    optimize as code

--- ali_sort/config/hostInfo.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  host: 'localhost',
-  port: 8000
-}
---
+++ ali_sort/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: 'localhost',
+  port: 8000,
+  // config yourself cookie, copy from try.taobao.com when you have login.
+  cookie: ''
+}
