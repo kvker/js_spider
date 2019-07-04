b7b3475e23edf6320ad30cbd0c5f92a7cc8330f4
zweizhao <zweizhao@foxmail.com>
2019-07-04 18:10:11 +0800

    优化部分代码

--- pixiv_col/main.js
+++ pixiv_col/main.js
@@ -3,16 +3,16 @@ const request = require('superagent')
 const fs = require('fs')
 const cheerio = require('cheerio')
 const exec = require('child_process').exec
-// start page
 let page = 1
-  , pageCurrentLength = 0
-  , pageMaxLength = 1
-  , total = 1446
+let pageCurrentLength = 0
+let pageMaxLength = 1
+let total = 0
 
-function restart() {
+/**
+ * 获取每一页的内容
+ */
+function getPageImgs() {
   pageCurrentLength = 0
-  // debugger
-  // if(page > 2) return
   request
     .get(`https://www.pixiv.net/bookmark.php?rest=show&p=${page}`)
     .set('cookie', config.cookie)
@@ -24,19 +24,17 @@ function restart() {
       const result = res.text
       $ = cheerio.load(result)
       let thumbnails = $('._layout-thumbnail>img')
-        , titles = $('a>h1.title')
+      let titles = $('a>h1.title')
+      // 获取总页数
+      if(!total) total = +$('.bookmark-tag-all')[0].firstChild.data.match(/\d+/)[0]
 
-      // max length base on thumbnails length
       pageMaxLength = thumbnails.length
       Array.from(thumbnails).forEach(async (thumbnail, index) => {
-        // debugger
-        // if(index) return
         let imgUrl = thumbnail.attribs['data-src']
-          , imgFix = imgUrl.match(/\.\w+$/)[0]
-          , imgTitle = titles[index].attribs.title
-          , imgName = imgTitle + imgFix
-          // normal rule with scale img and origin img.
-          , originImgUrl = imgUrl.replace(/\/c\/150x150\/img-master(.*?)_master1200/, '/img-original$1')
+        let imgFix = imgUrl.match(/\.\w+$/)[0]
+        let imgTitle = titles[index].attribs.title
+        let imgName = imgTitle + imgFix
+        let originImgUrl = imgUrl.replace(/\/c\/150x150\/img-master(.*?)_master1200/, '/img-original$1')
 
         checkLinkStatus({ imgTitle, imgName, originImgUrl }, index)
       })
@@ -48,11 +46,10 @@ function checkLinkStatus({ imgTitle, imgName, originImgUrl }, index) {
     .set('cookie', config.cookie)
     .set('referer', config.referer)
     .end((err, res) => {
-      // img's suffix maybe wrong. then should change to another one.(png and jpg)
       if(err) {
         let originImgUrlFix = originImgUrl.match(/\.\w+$/)[0]
-          , png = '.png'
-          , jpg = '.jpg'
+        let png = '.png'
+        let jpg = '.jpg'
         if(originImgUrlFix === png) {
           imgName = imgTitle + jpg
           originImgUrl = originImgUrl.replace(/\.\w+$/, jpg)
@@ -67,9 +64,8 @@ function checkLinkStatus({ imgTitle, imgName, originImgUrl }, index) {
 }
 
 function downloadFile({ imgTitle, imgName, originImgUrl }) {
-  // ignore '/' error
   let stream = fs.createWriteStream(`./pixiv_col/imgs/${imgName.replace(/\//g, '')}`)
-    , req = request.get(originImgUrl)
+  let req = request.get(originImgUrl)
       .set('cookie', config.cookie)
       .set('referer', config.referer)
 
@@ -78,10 +74,10 @@ function downloadFile({ imgTitle, imgName, originImgUrl }) {
   req.on('end', () => {
     console.log(pageCurrentLength + pageMaxLength * page - pageMaxLength + 1 + '/' + total)
     if(++pageCurrentLength >= pageMaxLength) {
-      restart(++page)
+      getPageImgs(++page)
       exec(`echo ${pageCurrentLength} >> pixiv_col/length.txt;echo ${page} >> pixiv_col/page.txt`)
     }
   })
 }
 
-restart()
+getPageImgs()
